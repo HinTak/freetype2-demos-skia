91cc64bb71b2a45018f89573c55f35d502004d71
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-05-09 00:39:24 +0100

    freetype2.diff should not be needed after freetype2 update

--- 00_apply-patches.sh
+++ 00_apply-patches.sh
@@ -4,9 +4,9 @@ pushd svg-native-viewer
   patch -p1 < ../svg-native-viewer.diff
 popd
 
-pushd freetype2
-  patch -p1 < ../freetype2.diff
-popd
+#pushd freetype2
+#  patch -p1 < ../freetype2.diff
+#popd
 
 pushd freetype2-demos
   patch -p1 < ../freetype2-demos-4-svg-hooks-skia.patch
